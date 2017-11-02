dd4c6f9d46df9219b4ae288de629c8efdaa98e3e
Edouard Lopez <contact@edouard-lopez.com>
2017-11-02 19:37:22 +0100

    list vocabulary

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <results-list></results-list>
   </div>
 </template>
 
 <script>
-import got from 'got';
-
-let vocabulary = got.get(
-  'https://raw.githubusercontent.com/edouard-lopez/lsf/master/src/assets/vocabulaire.json',
-  { json: true }
-);
-console.log(vocabulary);
+import resultsList from './components/results-list.vue';
 
 export default {
   name: 'app',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    };
+  components: {
+    'results-list': resultsList
   }
 };
 </script>
@@ -46,23 +23,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
---
+++ src/components/results-list.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Résultats</h1>
+    <ul id="resultats">
+      <li v-for="mot in vocabulaire">
+        <a :href="mot.url">
+          {{ mot.mot }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import got from 'got';
+
+export default {
+  data: function() {
+    return { vocabulaire: [] };
+  },
+  created: function() {
+    got
+      .get(
+        'https://raw.githubusercontent.com/edouard-lopez/lsf/master/src/assets/vocabulaire.json',
+        { json: true }
+      )
+      .then(response => {
+        this.vocabulaire = response.body;
+      });
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

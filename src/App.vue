d82309d977b24220c346364c53170ac754601797
Edouard Lopez <contact@edouard-lopez.com>
2017-11-15 23:51:34 +0100

    format code

--- src/App.vue
+++ src/App.vue
@@ -25,92 +25,102 @@
 </template>
 
 <script>
-import got from 'got';
-import dataset from './assets/vocabulaire.json';
-import videojs from 'video.js'
-import {highlight} from './filters.js';
+  import got from 'got';
+  import dataset from './assets/vocabulaire.json';
+  import videojs from 'video.js'
+  import {highlight} from './filters.js';
 
-
-export default {
-  name: 'app',
-  data: function() {
-    return {
-      search: '',
-      vocabulaire: [],
-      videoPlaying: {label: undefined}
-    };
-  },
-  created: function() {
-    this.vocabulaire = dataset;
-  },
-  mounted: function () {
-    this.player = videojs('player', {
-      controls: false,
-      autoplay: true,
-      loop: true,
-      fluid: true,
-      sources: [{src: '', type: 'video/webm'}]
-    });
-  },
-    computed: {
-    count: function() {
-      return this.filteredMots.length;
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        search: '',
+        vocabulaire: [],
+        videoPlaying: {label: undefined}
+      };
     },
-    filteredMots: function() {
-      return this.vocabulaire.filter(
-        mot =>
-          mot.label.indexOf(this.search) !== -1 ||
-          mot.key.indexOf(this.search) !== -1
-      );
+    created: function () {
+      this.vocabulaire = dataset;
+    },
+    mounted: function () {
+      this.player = videojs('player', {
+        controls: false,
+        autoplay: true,
+        loop: true,
+        fluid: true,
+        sources: [{src: '', type: 'video/webm'}]
+      });
+    },
+    computed: {
+      count: function () {
+        return this.filteredMots.length;
+      },
+      filteredMots: function () {
+        return this.vocabulaire.filter(
+          mot =>
+            mot.label.indexOf(this.search) !== -1 ||
+            mot.key.indexOf(this.search) !== -1
+        );
+      }
+    },
+    methods: {
+      play: function (mot) {
+        this.videoPlaying = mot
+        this.player.src(this.videoPlaying.video)
+        this.player.play();
+      }
+    },
+    filters: {
+      highlight
     }
-  },
-  methods: {
-  	play: function (mot) {
-  	  this.videoPlaying = mot
-      this.player.src(this.videoPlaying.video)
-      this.player.play();
-    }
-  },
-  filters: {
-    highlight
-  }
-};
+  };
 </script>
 
 <style lang="scss">
-$icon-font-path: '~videojs-font/fonts';
-@import '~video.js/dist/video-js.css';
-body {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  background-color: #FDFDFD;
-  margin: 0;
-}
+  $icon-font-path: '~videojs-font/fonts';
+  @import '~video.js/dist/video-js.css';
 
-a:active, a:focus, a:hover {
-  color: #c13c27;
-}
-a {
-  color: #dd6a58;
-  transition: color 0.1s linear;
-}
+  body {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    background-color: #FDFDFD;
+    margin: 0;
+  }
 
-.header {
-  background-color: #E04E39;
-  color: white;
-  padding: .5rem;
-}
-h1 { padding: 0; margin: 0}
-li { line-height: 1.5rem;}
-.player {
-  display:block;
-  width:425px;
-  height:300px;
-}
-.highlight {
-  background-color: #F8E7CF;
-  color: #444;
-}
+  a:active, a:focus, a:hover {
+    color: #c13c27;
+  }
+
+  a {
+    color: #dd6a58;
+    transition: color 0.1s linear;
+  }
+
+  .header {
+    background-color: #E04E39;
+    color: white;
+    padding: .5rem;
+  }
+
+  h1 {
+    padding: 0;
+    margin: 0
+  }
+
+  li {
+    line-height: 1.5rem;
+  }
+
+  .player {
+    display: block;
+    width: 425px;
+    height: 300px;
+  }
+
+  .highlight {
+    background-color: #F8E7CF;
+    color: #444;
+  }
 </style>

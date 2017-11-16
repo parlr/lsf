4afae4c522234551df824c9fcaa863ce62c64658
Edouard Lopez <contact@edouard-lopez.com>
2017-11-16 02:39:51 +0100

    generate index.html during build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -77,6 +78,7 @@ if (process.env.NODE_ENV === 'production') {
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
-    })
+    }),
+    new HtmlWebpackPlugin()
   ]);
 }

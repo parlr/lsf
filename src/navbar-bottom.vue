bfc77174a7e650c2bd5ba22d27a04afb64e85eb8
Edouard Lopez <contact@edouard-lopez.com>
2017-11-28 01:55:38 +0100

    fix webpack config for dev/prod

--- src/navbar-bottom.vue
+++ src/navbar-bottom.vue
@@ -2,21 +2,28 @@
   <nav class="navbar is-fixed-bottom is-light">
     <div class="navbar-brand has-text-weight-bold">
       <span class="navbar-item" @click="showIndex=true">
-        <img src="/dist/list.svg" class="fa" alt="">
+        <img :src="listIcon" class="fa" alt="">
         Liste
       </span>
       <span class="navbar-item" @click="random()">
-        <img src="/dist/random.svg" class="fa" alt="">
+        <img :src="randomIcon" class="fa" alt="">
         Aléatoire
       </span>
     </div>
   </nav>
 </template>
 <script>
-  import 'font-awesome-svg-png/black/svg/random.svg'
-  import 'font-awesome-svg-png/black/svg/list.svg'
+  import randomIcon from 'font-awesome-svg-png/black/svg/random.svg'
+  import listIcon from 'font-awesome-svg-png/black/svg/list.svg'
 
-  export default {}
+  export default {
+    data: function () {
+      return {
+        randomIcon,
+        listIcon
+      }
+    },
+  }
 </script>
 <style scoped>
   .fa {
--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,12 @@ const webpack = require("webpack");
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "./",
+    path: path.resolve(__dirname, "dist"),
     filename: "build.js"
   },
   plugins: [
@@ -18,51 +17,32 @@ module.exports = {
   ],
   module: {
     rules: [
+      { test: /\.vue$/, loader: "vue-loader" },
+      { test: /\.(eot|ttf|woff|woff2)$/, loader: "url-loader" },
+      { test: /\.js$/, loader: "babel-loader" },
+      { test: /\.(svg|ico)$/, loader: "file-loader" },
+      { test: /manifest\.json$/, loader: "file-loader", options: {name: '[name].[ext]'} },
       {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract({
           fallback: "style-loader",
-          use: ['css-loader', 'sass-loader'],
-          publicPath: ""
+          use: ["css-loader", "sass-loader"],
         })
-      },
-      {
-        test: /\.vue$/,
-        loader: "vue-loader"
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
-        loader: "url-loader"
-      },
-      { test: /\.js$/, loader: "babel-loader" },
-      {
-        test: /\.(png|jpg|gif|svg|ico)$/,
-        loader: "file-loader",
-        options: { name: "[name].[ext]?[hash]" }
       }
     ]
   },
   resolve: { alias: { vue$: "vue/dist/vue.runtime.min.js" } },
-  devServer: { overlay: true, publicPath: "/dist/" }
+  devServer: { overlay: true }
 };
 
 if (process.env.NODE_ENV === "production") {
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({ "process.env": { NODE_ENV: '"production"' } }),
-    new UglifyJSPlugin({
-      uglifyOptions: {
-        beautify: false,
-        ecma: 6,
-        compress: { warnings: false },
-        comments: false
-      }
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "index.html")
-    }),
+    new UglifyJSPlugin({ uglifyOptions: { beautify: false, ecma: 6 } }),
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, "index.html") }),
     new CopyWebpackPlugin([
-      {from: './src/assets/manifest.json'},
-      {from: './src/assets/images/**/icon-*', to: './images', flatten: true}
+      { from: "./src/assets/manifest.json" },
+      { from: "./src/assets/images/**/icon-*", to: "./images", flatten: true }
     ])
   ]);
 }

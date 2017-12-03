e5105c5d5eee35306e140cc2287000139acd5aea
Edouard Lopez <contact@edouard-lopez.com>
2017-12-03 10:16:41 +0100

    extract timing before splitting video

--- src/split.bash
+++ src/split.bash
@@ -2,26 +2,31 @@
 # see: https://askubuntu.com/q/56022/22343
 # REQUIREMENT:
 #   sudo apt install --yes ffmpeg libav-tools
+# USAGE
+#   bash -x ./split.bash "$video" ["$timing"]
 
 INPUT_FILE="$1"   # video file to split
-SPLIT_DATA="$2"   # tsv file: mot  start_time end_time
 
 full_path="$1"
 directory_path="${full_path%/*}"
+filename="${full_path##*/}"
 extension="${full_path##*.}"
+timing_path="${2:-$directory_path/$filename.tsv}"   # timing, as: mot  start_time end_time
 
-# awk 'BEGIN{FS=","} /Dialogue/{print $2" "$3}' \
-#   "$directory_path/${file/.$extension/.ass}" \
-# > "$directory_path/$file.tsv"
 
-FFMPEG() {
+extract_timing_from_subtitles() {
+  awk 'BEGIN{FS=","} /Dialogue/{print $2" "$3}' \
+     "$directory_path/${filename/.$extension/.ass}" \
+   > "$directory_path/$filename.tsv"
+}
+
+extract_word_chunk() {
   ffmpeg \
     -i "$INPUT_FILE" \
     -ss "$start" \
     -to "$end" \
     -vcodec copy \
     -acodec copy \
-    -cpu-used -4 \
     -loglevel error \
     "$chunk" < /dev/null
 }
@@ -37,7 +42,8 @@ convert_to_webm() {
     -minrate 128k \
     -maxrate 256k \
     -bufsize 192k \
-    -cpu-used -4 \
+    -quality good \
+    -cpu-used 2 \
     -deadline best \
     -loglevel error \
   "$chunk.webm" < /dev/null
@@ -49,9 +55,10 @@ split_video() {
     mot="${start}"
     chunk="$directory_path/$mot.$extension"
 
-    FFMPEG "${start}" "${end}"
+    extract_word_chunk "${start}" "${end}"
     convert_to_webm
-  done < $SPLIT_DATA
+  done < "$timing_path"
 }
 
+extract_timing_from_subtitles
 split_video

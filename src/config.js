a0e1ea18ffe1eaa40f8a95ad827ed3f806402d54
Edouard Lopez <contact@edouard-lopez.com>
2018-01-20 23:38:44 +0100

    add local dataset
    Live data are the one from
    https://raw.githubusercontent.com/parlr/lsf-data/master/vocabulaire.json

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   dataset: function () {
     let dataurl = '/dist/vocabulaire.json'
     if (process.env.NODE_ENV === "production") {
-      dataurl = "https://raw.githubusercontent.com/parlr/lsf-data/master/src/assets/vocabulaire.json"
+      dataurl = "https://raw.githubusercontent.com/parlr/lsf-data/master/vocabulaire.json"
     }
     return dataurl
   }

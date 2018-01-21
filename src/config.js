6ae1f3bf4710934ffc1cdbd08aba19e0a1956f89
Edouard Lopez <contact@edouard-lopez.com>
2018-01-21 03:36:54 +0100

    fix path to vocabulaire in dev

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   dataset: function () {
-    let dataurl = '/dist/vocabulaire.json'
+    let dataurl = "/vocabulaire.json";
     if (process.env.NODE_ENV === "production") {
       dataurl = "https://raw.githubusercontent.com/parlr/lsf-data/master/vocabulaire.json"
     }

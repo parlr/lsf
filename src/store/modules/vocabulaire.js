af74047f600bbb1eabd240b65d8ae17ec27b9079
Edouard Lopez <contact@edouard-lopez.com>
2018-11-23 12:58:16 +0100

    create vuex module 'vocabulaire'

---
+++ src/store/modules/vocabulaire.js
@@ -0,0 +1,15 @@
+const state = {
+  vocabulaire: []
+};
+
+const getters = {};
+const actions = {};
+const mutations = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+};

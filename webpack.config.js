f69e42cce0c459a781d3871e17d1aa04907b3592
Edouard Lopez <contact@edouard-lopez.com>
2018-11-23 12:49:38 +0100

    fix path to entry in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-  entry: [path.resolve(__dirname, 'src') + 'index.js'],
+  entry: [path.join(path.resolve(__dirname), 'src', 'index.js')],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'build.js'

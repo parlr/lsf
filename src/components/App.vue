86d0325083428fc6e5c072440a3adafa5c76e339
Edouard Lopez <contact@edouard-lopez.com>
2018-11-23 16:46:22 +0100

    migrate vocabulaire management to store

--- src/components/App.vue
+++ src/components/App.vue
@@ -3,9 +3,9 @@
     <nav class="navbar is-fixed-top is-primary" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <div class="navbar-item control search">
-          <input v-model="search"
+          <input v-model="searchQuery"
                  @click="focus()"
-                 @keyup="displayIndex()"
+                 @keyup="search()"
                  @keyup.enter="playFirst()"
                  id="search"
                  placeholder="comment signer…"
@@ -14,29 +14,31 @@
                  autocomplete="off"
                  type="text">
         </div>
-        <label for="search" class="navbar-item is-paddingless is-hidden-mobile">{{count}} Résultats</label>
+        <label for="search" class="navbar-item is-paddingless is-hidden-mobile">
+          {{matchingCount}} Résultats
+          </label>
       </div>
     </nav>
 
     <main class="content">
       <div class="container is-fluid">
         <main class="tile is-ancestor layout">
-          <nav v-if="showIndex" class="tile is-vertical is-2">
+          <nav v-if="showEntries" class="tile is-vertical is-2">
             <div class="tile is-parent index" role="navigation" aria-label="words index">
               <div class="tile is-child">
-                <list-placeholder v-if="loadingData"></list-placeholder>
+                <list-placeholder v-if="isLoading"></list-placeholder>
                 <ul class="index_content is-unstyled">
                   <li v-for="mot in filteredMots" v-bind:key="mot.key" class="index_content--item has-bottom-margin">
                     <a href="#top" @click="play(mot)"
                        :id="mot.key"
-                       :inner-html.prop="mot.label | highlight(search)">
+                       :inner-html.prop="mot.label | highlight(searchQuery)">
                     </a>
                   </li>
                 </ul>
               </div>
             </div>
           </nav>
-          <aside v-if="!loadingData" class="tile is-parent video">
+          <aside v-if="!isLoading" class="tile is-parent video">
             <figure class="tile is-child">
               <figcaption>
                 <video :src="videoUrl"
@@ -61,12 +63,12 @@
 </template>
 
 <script>
-import http from 'ky';
 import config from '~/config';
 import { highlight } from '~/filters';
 import '~/assets/lsf.scss';
 import navbarBottom from '~/components/navbar-bottom.vue';
 import listPlaceholder from '~/components/list-placeholder.vue';
+import { mapState, mapGetters } from 'vuex';
 
 export default {
   components: {
@@ -76,52 +78,38 @@ export default {
   name: 'app',
   data: function() {
     return {
-      search: '',
-      showIndex: true,
-      vocabulaire: [],
+      searchQuery: '',
+      showEntries: true,
       videoPlaying: { label: undefined, video: '' }
     };
   },
   created: function() {
-    (async () => {
-      try {
-        this.vocabulaire = await http.get(config.dataset(), {}).json();
-      } catch (error) {
-        console.error(err);
-      }
-    })();
+    this.$store.dispatch('vocabulaire/fetchAll');
   },
   mounted: function() {
     this.player = document.querySelector('video');
   },
   computed: {
-    loadingData: function() {
-      return this.vocabulaire.length === 0;
-    },
-    count: function() {
-      return this.filteredMots.length;
-    },
-    filteredMots: function() {
-      return this.vocabulaire.filter(
-        mot =>
-          mot.label.indexOf(this.search) !== -1 ||
-          mot.key.indexOf(this.search) !== -1
-      );
-    },
     videoUrl: function() {
       return `${config.cdn}/${this.videoPlaying.video}`;
-    }
+    },
+    ...mapGetters('vocabulaire', ['filteredMots', 'matchingCount']),
+    ...mapState('vocabulaire', ['isLoading'])
   },
   methods: {
     focus: function() {
       window.scrollTo(0, 0);
     },
-    displayIndex: function() {
-      this.showIndex = true;
+    search: function() {
+      this.$store.dispatch({
+        type: 'vocabulaire/updateSearch',
+        search: this.searchQuery
+      });
+      this.showEntries = true;
     },
     play: function(mot) {
-      this.showIndex = false;
-      this.search = mot.label;
+      this.showEntries = false;
+      this.searchQuery = mot.label;
       this.videoPlaying = mot;
     },
     playFirst: function() {

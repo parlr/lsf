37b59eb67bdc7487c24350f248bd90ccf1f2c19a
Edouard Lopez <contact@edouard-lopez.com>
2018-11-23 12:57:47 +0100

    setup vuex

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
+
+export default new Vuex.Store({
+  modules: {},
+
+  strict: debug,
+  plugins: debug ? [createLogger()] : []
+});

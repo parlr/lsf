4e283cae67819433e70a0e77e49ac892deeb517a
Edouard Lopez <contact@edouard-lopez.com>
2018-11-23 13:35:49 +0100

    add 'player' module to store

---
+++ src/store/modules/player.js
@@ -0,0 +1,16 @@
+const state = {
+  playing: false,
+  videoUrl: null
+};
+
+const getters = {};
+const actions = {};
+const mutations = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+};

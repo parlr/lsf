0083ac20144975bd01a5f39434f58390019ad082
Edouard Lopez <contact@edouard-lopez.com>
2018-11-23 12:06:02 +0100

    add v-for key binding

--- src/list-placeholder.vue
+++ src/list-placeholder.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="index_content is-unstyled">
-    <li v-for="placeholder in placeholders" class="index_content--item has-bottom-margin">
+    <li v-for="placeholder in placeholders" :key="placeholder" class="index_content--item has-bottom-margin">
       <span class="placeholder">{{placeholder}}</span>
     </li>
   </ul>

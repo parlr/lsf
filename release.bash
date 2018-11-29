07527cb1343645d0a9c895a57f64b714c122e52d
Edouard Lopez <contact@edouard-lopez.com>
2018-11-29 01:29:47 +0100

    revamp release script

--- release.bash
+++ release.bash
@@ -1,10 +1,36 @@
 #!/usr/bin/env bash
 
-git checkout gh-pages
-git reset --hard master
-yarn build
-git add dist/
-yarn version --new-version "$1"
-git push --force
-git push --tags
-git checkout master
+version="$1"
+
+function commit_app() {
+    local version="$version"
+
+    git checkout master -- dist/
+    mv dist/* ./
+    git add .
+    git commit -m "deploy $version"
+}
+
+function create_build() {
+    rm dist/ -rf
+    yarn install --frozen-lockfile
+    yarn build
+}
+
+function create_release() {
+    local version="$version"
+
+    yarn version --new-version "$version"
+}
+
+function deploy() {
+    git push
+    git push --tags
+}
+
+create_build \
+    && create_release "$version" \
+    && git checkout gh-pages \
+    && commit_app "$version" \
+    && deploy \
+    && git checkout master

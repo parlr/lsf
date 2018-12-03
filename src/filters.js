bb8fa054a4fc8c6a3cbf08aa8a0604a276911ea7
Edouard Lopez <contact@edouard-lopez.com>
2018-12-04 00:04:05 +0100

    escape searchQuery when using it in a Regexp
    fixes #10

--- src/filters.js
+++ src/filters.js
@@ -1,10 +1,16 @@
-export { highlight };
+export { highlight, regExpEscape };
 
 function highlight(content, query) {
   if (query.trim().length === 0) return content;
 
-  var search = new RegExp(query, 'ig');
+  var search = new RegExp(regExpEscape(query), 'ig');
   return content
     .toString()
     .replace(search, match => `<span class="highlight">${match}</span>`);
+}
+
+// see: https://stackoverflow.com/a/3561711/802365
+function regExpEscape(string) {
+  console.log('escape', string);
+  return String(string).replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 }
--- src/store/modules/vocabulaire/getters.js
+++ src/store/modules/vocabulaire/getters.js
@@ -1,8 +1,10 @@
+import { regExpEscape } from '~/filters';
+
 export default {
   filteredMots: state =>
     state.vocabulaire.filter(mot => {
       const NOT_FOUND = -1;
-      let query = new RegExp(state.searchQuery, 'i');
+      let query = new RegExp(regExpEscape(state.searchQuery), 'ig');
 
       return mot.key.search(query) !== NOT_FOUND;
     }),

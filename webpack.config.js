5d42da96a4f8499434e7d00587ba821218857b23
Edouard Lopez <contact@edouard-lopez.com>
2018-12-18 11:17:52 +0100

    fix path to data included when in dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,6 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"development"' } }),
-    new CopyWebpackPlugin([{ from: '../lsf-data/vocabulaire.json', to: './' }])
+    new CopyWebpackPlugin([{ from: './src/assets/vocabulaire.json', to: './' }])
   ]);
 }
